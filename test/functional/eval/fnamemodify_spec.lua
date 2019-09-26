b79f3fac923eec095bfe514b84395cce958b3b39
Rob Pilling <robpilling@gmail.com>
2019-09-26 23:04:59 +0100

    Add more functional fnamemodify() tests

--- test/functional/eval/fnamemodify_spec.lua
+++ test/functional/eval/fnamemodify_spec.lua
@@ -6,6 +6,8 @@ local fnamemodify = helpers.funcs.fnamemodify
 local command = helpers.command
 local write_file = helpers.write_file
 
+-- see unit tests for fnamemodify() tests that don't use cwd
+
 describe('fnamemodify()', function()
   setup(function()
     write_file('Xtest-fnamemodify.txt', [[foobar]])
@@ -17,7 +19,7 @@ describe('fnamemodify()', function()
     os.remove('Xtest-fnamemodify.txt')
   end)
 
-  it('works', function()
+  it('handles the root path', function()
     local root = helpers.pathroot()
     eq(root, fnamemodify([[/]], ':p:h'))
     eq(root, fnamemodify([[/]], ':p'))
@@ -35,5 +37,43 @@ describe('fnamemodify()', function()
 
   it(':8 works', function()
     eq('Xtest-fnamemodify.txt', fnamemodify([[Xtest-fnamemodify.txt]], ':8'))
+  end)
+
+  describe('examples from ":help filename-modifiers"', function()
+    local filename = "src/version.c"
+    local cwd = helpers.getcwd()
+
+    it(':p', function()
+      eq(cwd .. '/src/version.c', fnamemodify(filename, ':p'))
+    end)
+
+    it(':p:.', function()
+      eq('src/version.c', fnamemodify(filename, ':p:.'))
+    end)
+
+    it(':p:h', function()
+      eq(cwd .. '/src', fnamemodify(filename, ':p:h'))
+    end)
+
+    it(':p:h:h', function()
+      eq(cwd .. '', fnamemodify(filename, ':p:h:h'))
+    end)
+
+    it(':p:t', function()
+      eq('version.c', fnamemodify(filename, ':p:t'))
+    end)
+
+    it(':p:r', function()
+      eq(cwd .. '/src/version', fnamemodify(filename, ':p:r'))
+    end)
+
+    it(':s?version?main?:p', function()
+      eq(cwd .. '/src/main.c', fnamemodify(filename, ':s?version?main?:p'))
+    end)
+
+    it(':p:gs?/?\\\\?', function()
+      local converted_cwd = cwd:gsub('/', '\\')
+      eq(converted_cwd .. '\\src\\version.c', fnamemodify(filename, ':p:gs?/?\\\\?'))
+    end)
   end)
 end)
